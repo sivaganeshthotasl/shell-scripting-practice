3535cf56486f1e5e83a09cbc6346a179d6c38e0b
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-05 20:11:08 +0530

    values variables

--- task4-variable.sh
+++ task4-variable.sh
@@ -7,5 +7,8 @@ name="Siva"
 echo "my name is $name"
 echo 'my name is $name' # this state ment only prints $name becuase of single qutation.
 
+echo "My name is $name"
+echo '$name is learning Devsecops"
+
 
 
