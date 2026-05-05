8c942b10dabfbc80b32cfe08d8792eb48d2c9971
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-05 15:57:44 +0530

    modified

--- 2gpt-echo.sh
+++ 2gpt-echo.sh
@@ -3,7 +3,18 @@
 #Using -e (new line) \n(new line)
 
 
-echo -e "Hello\nSivaganeshthotasl"
+#echo -e "Hello\nSivaganeshthotasl"
 
 
-echo -e "Hello\tSiva"
+#echo -e "Hello\tSiva"
+
+
+
+echo -e "A\nB"
+
+echo -e "A\tB"
+
+echo -e "A\\B"
+
+echo -e "A\B"
+
