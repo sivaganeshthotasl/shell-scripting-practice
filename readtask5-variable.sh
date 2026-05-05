06b2c7dcd1308b2995f3b18f26668460d91e6592
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-05 21:43:56 +0530

    learning read variables for user input

--- readtask5-variable.sh
+++ readtask5-variable.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-read -p "Enter your name:" name
-read -p "Enter your role:" role
+#read -p "Enter your name:" name
+#read -p "Enter your role:" role
 
-echo "$name working as $role"
+#echo "$name working is as $role"
+
+read -p "Enter you name and role:" name role
+
+echo "$name working is as $role"
 
