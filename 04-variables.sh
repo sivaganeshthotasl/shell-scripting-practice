2118b8a6dec8da81ab78c5a9e88656fe031d6053
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-05 12:36:00 +0530

    Modified changes regarding arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hey $PERSON2, How are you doing today?"
+echo "$PERSON2: hello $PERSON1, i am doing great and you"
+echo "$PERSON1: i am good, thank you."
+echo "$PERSON2: sounds great"
