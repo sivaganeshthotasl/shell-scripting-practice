8eab437decce059f27117c4b10e08e1d17c53290
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-05 11:56:09 +0530

    conversation between Elon and Trump

---
+++ 02-convesation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Elon, How are you doing today?"
+echo "Elon: i am doing great, thank you for asking. What about you?"
+echo "Trump: i am also doing good."
+echo "Elon: Fantastic.
