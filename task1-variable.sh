afd13132af27bd469cc32e1f0b8ed0cd0d0c385e
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-05 19:52:55 +0530

    About name and city

---
+++ task1-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create a variable, you name : and your city: . Print My name is siva and i live in banglore.
+
+
+name="Siva"
+city="Banglore"
+
+echo "My name is $name"
+echo "i live in $city"
+
