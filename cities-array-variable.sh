ca6ad9caab3c10906fa33943b8af3735a3b333db
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-06 12:11:03 +0530

    array variables learning

--- cities-array-variable.sh
+++ cities-array-variable.sh
@@ -6,3 +6,11 @@ cities=("Bangalore" "Hyderabad" "Chennai" "Amaravathi")
 
 echo ${cities[@]}
 
+#Print second city
+
+cities=("Bangalore" "Hyderabad" "Chennai" "Amaravathi")
+
+echo ${cities[1]}  # it will pick 2nd index of city and print
+
+
+
