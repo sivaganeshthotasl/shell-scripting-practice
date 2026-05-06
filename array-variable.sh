7c23d45819e89ba7028d03615b16764180d6b6a8
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-06 11:13:18 +0530

    array variables learning

--- array-variable.sh
+++ array-variable.sh
@@ -2,11 +2,12 @@
 
 names=("Siva" "Ravi" "Ganesh")
 
-echo ${names [0]}
-echo ${names [1]}
-echo ${names [2]}
+echo ${names[0]}
+echo ${names[1]}
+echo ${names[2]}
 
 
+#wrong syntax
 #echo ${names [3]}
 #echo ${names [4]}
 #echo ${names [5]}
