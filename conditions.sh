39a3d54ac0836838af568a615b391fd8d1287973
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-09 21:41:43 +0530

    conditions are learning

--- conditions.sh
+++ conditions.sh
@@ -6,6 +6,6 @@ if [ $Number -gt 10 ]
 then
      echo " Given Number is greater than 10 "
 else
-     echo " Given Number is not equal than 10 "
+     echo " Given Number is not greater than 10 "
 fi
 
