f39f854634335619f6ee1eaa8078bfca00e60f8f
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-09 22:35:27 +0530

    conditions are learning

--- install-command.sh
+++ install-command.sh
@@ -11,13 +11,30 @@ else
      echo " you are running with root user "
 fi
 
+dnf list installed mysql
 
-dnf install mysql -y
-
-if [ $? -eq 0 ]
+if [ $? -ne 0]
 then
-     echo " Installing Mysql is ..... Successfull "
+     echo " Mysql is not installed ..... so going to install it "
+     dnf install mysql -y
+if [ $? -eq 0 ]
+then 
+     echo " Install Mysql ...... is Successfull "
 else
-     echo " Installing Mysql is ..... Failure "
+     echo " installing mysql is ::::: Failure "
      exit 1
+else
+     echo " Mysql is already install:: nothing to do "
+     
 fi
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then
+#      echo " Installing Mysql is ..... Successfull "
+# else
+#      echo " Installing Mysql is ..... Failure "
+#      exit 1
+# fi
+
