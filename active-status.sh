c357d8b954a791b3e4a0a22564cc7033c911dc22
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-10 11:03:32 +0530

    Larning Conditions

--- active-status.sh
+++ active-status.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+     echo " Error:: Please proceed with root user "
+     exit 1
+else 
+     echo " You are using root user "
+fi
+
 
 systemctl is-active sshd
 
