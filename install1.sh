8ef68411fdc3822fa1ef96b67b103431ac844ec9
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-10 10:11:09 +0530

    Larning Conditions

---
+++ install1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+     echo " Error:: Please proceed with root user "
+else
+     echo " You are using Root User "
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+     echo " Mysql is not installed..... going to Install"
+     dnf install mysql
+     
+if [ $? -eq 0 ]
+then
+     echo " Mysql is installing...... Successfull "
+else
+     echo " Installing Mysql::::::: Failure "
+     exit 1
+
+else
+     echo " Mysql is already installed::: Nothing to do "
+fi
+
+
+
+
+     
