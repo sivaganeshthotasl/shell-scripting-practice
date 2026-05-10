10db23feab80fef918166af8f4f2768cf63574d2
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-10 10:29:22 +0530

    Larning Conditions

--- ifelse.sh
+++ ifelse.sh
@@ -5,8 +5,8 @@ number=10
 
 if [ $number -eq 10 ]
 then
-     echo " $number is equal to 10 "
+     echo " number is equal to 10 "
 else
-     echo " $number is not qual to 10 "
+     echo " number is not qual to 10 "
 fi
 
