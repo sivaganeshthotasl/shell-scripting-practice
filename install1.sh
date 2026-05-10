96c5f973198f786b98b6acd61324158656068a8e
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-10 10:17:58 +0530

    Larning Conditions

--- install1.sh
+++ install1.sh
@@ -17,7 +17,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
      echo " Mysql is not installed..... going to Install"
-     dnf install mysql
+     dnf install mysql -y
      
 if [ $? -eq 0 ]
 then
