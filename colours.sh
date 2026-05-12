86b626ed0f6ebdb929199b363c950abc1061191f
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-12 12:53:30 +0530

    learning colours

--- colours.sh
+++ colours.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+     echo " \e[31m ERROR:: you are not running script with root user "
+else
+     echo " \e[32m You are running with Root user "
+fi
+
 echo -e " \e[31m Hello Red Colur "
 
 echo " Hello No Colour "
