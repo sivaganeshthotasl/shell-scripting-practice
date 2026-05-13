30b2f952d8b01bf5f4bd8eadf57a259660f3e21c
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-13 11:19:41 +0530

    Colours leanring

--- redirect.sh
+++ redirect.sh
@@ -6,6 +6,6 @@ Y="\e[33m
 B="\e[34m"
 N="\e[0m"
 
-echo -e " $G Hello DevSecops " > file.txt
-echo -e " $B Hello Siva Ganesh $N You are working as $Y DevSecOps Engineer  $N" right $N " >> file.txt
+echo -e " $G Hello DevSecops $N " > file.txt
+echo -e " $B Hello Siva Ganesh $N You are working as $Y DevSecOps Engineer $N right $N " >> file.txt
 
