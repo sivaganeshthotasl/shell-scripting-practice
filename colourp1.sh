7360e09888324dcc0f80c5b4c0394238d390f885
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-13 10:45:31 +0530

    Colours leanring

--- colourp1.sh
+++ colourp1.sh
@@ -6,6 +6,15 @@ Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
 
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+     echo -e " $R You are not running this script with Root User $N "
+     exit 1
+else
+     echo -e " $G You are running with $G Root User $N "
+fi 
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
