2a300d85aa98b0373a7966c86e16bfad8deb1284
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-13 10:29:53 +0530

    Colours leanring

--- coloursp.sh
+++ coloursp.sh
@@ -31,9 +31,26 @@ echo -e "Hello Ganesh You are going to Complete shell scripting \e[32m Successfu
 
 #Lets create some colours code using some colour codes
 
-echo -e " This Is \e[31m RED \e[0m C0lour "
-echo -e " This Is \e[32m Green \e[0m Colour "
-echo -e " This is \e[33m Yellow \e[0m Colour "
+echo -e " This Is \e[31m RED \e[0m C0lour "    # what is \e[0m means it resets everthing back to normal colour.
+echo -e " This Is \e[32m Green \e[0m Colour "  # \e[0m it means it resets everthing back to normal colour. 
+echo -e " This is \e[33m Yellow \e[0m Colour " # \e[0m is means it resets everything back to normal.
+
+
+
+echo -e " \e[35m Proffessional method using VARIABLES \e[0m "
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B=\e[34m"
+N="\e[om"
+
+echo -e "$R ERROR $N"
+echo -e "$G SUCCESS $N"
+echo -e "$Y WARNING $N"
+echo -e "$B INFO $N "
+
+
 
 
 
