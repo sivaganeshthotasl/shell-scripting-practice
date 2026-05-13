b5e4a6d365d139e5f7693bd71f54583b1752a203
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-13 10:04:52 +0530

    Colours leanring

---
+++ coloursp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Why colours are User
+
+#Normally terminal outpu looks plain like
+
+# Installatino Completed
+# Installation Failed
+# Server Down
+
+#so we use colours to highlight those. lets begin 
+
+echo -e "Hello You are in "\e[31m" Danger "\e[0m""
+
