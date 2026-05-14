b38695727625d5feff70da14a847dac46e8362aa
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-14 15:18:43 +0530

    Learning For loop

--- forloopp4.sh
+++ forloopp4.sh
@@ -2,21 +2,13 @@
 
 # Installing packages using for loop
 
-LOG_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-
-mkdir -p "$LOG_FOLDER"
-echo " script started executed at $(date) " | tee -a $LOG_FOLDER
-
-
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-     echo " You are not using root user to run this script " | tee -a $LOG_FOLDER
+     echo " You are not using root user to run this script " 
      exit 1
 else
-     echo " you are running with root user " | tee -a $LOG_FOLDER
+     echo " you are running with root user " 
 fi
 
 
@@ -25,9 +17,9 @@ do
 dnf install $package -y &>>$LOG_FOLDER
 if [ $? -eq 0 ]
 then
-     echo -e " \e[34m $package \e[0m is installed \e[32m Successfull \e[0m " | tee -a $LOG_FOLDER
+     echo -e " \e[34m $package \e[0m is installed \e[32m Successfull \e[0m " 
 else
-     echo -e " \e[34m $package \e[0m installation is \e[31m Failed \e[0m " | tee -a $LOG_FOLDER
+     echo -e " \e[34m $package \e[0m installation is \e[31m Failed \e[0m " 
      exit 1
 fi
 done
