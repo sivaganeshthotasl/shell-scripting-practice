59371e9e7f65dac73503b38d044f7c1e56f7ab6e
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-14 14:49:56 +0530

    Learning For loop

---
+++ forloopp4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Installing packages using for loop
+
+for package in (nginx mysql)
+do
+dnf install $packages -y
+if [ $? -eq 0 ]
+then
+     echo -e " \e[34m $package \e[0m is installed \e[32m Successfull \e[0m "
+else
+     echo -e " \e[34m $package \e[0m installation is \e[31m Failed \e[0m "
+fi
+done
