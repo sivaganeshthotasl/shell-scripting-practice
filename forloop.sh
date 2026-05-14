359d6aa715fadeaef5223f9ddf3f39f6f0bce6dd
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-15 04:55:17 +0530

    Learning Function

--- forloop.sh
+++ forloop.sh
@@ -48,7 +48,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
      echo " $2 is not found....Going to Install it "
-     dnf install mysql -y
+     dnf install mysql -Y
      VALIDATE $? "mysql"
 else
      echo " $2 is already Installed....: Nothing to do "
