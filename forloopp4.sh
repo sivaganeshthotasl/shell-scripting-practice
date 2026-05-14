9fe2b71eb55cf8914a475e3412760e4bfd53b561
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-14 15:13:00 +0530

    Learning For loop

--- forloopp4.sh
+++ forloopp4.sh
@@ -6,7 +6,7 @@ LOG_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
-mkidr -p "$LOG_FOLDER"
+mkdir -p "$LOG_FOLDER"
 echo " script started executed at $(date) "
 
 
