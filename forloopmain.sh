dbf04d679e240689cd22f05dc2723e8f70cc866d
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-15 15:13:53 +0530

    Learning Function

--- forloopmain.sh
+++ forloopmain.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 LOG_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
-LOG_FILE="LOG_FOLDER/SCRIPT_NAME.log"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p "$LOG_FOLDER"
 echo " Script is executing started at $(date) " 
