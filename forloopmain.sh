289f35584e7dc9d3a86221a188f7b618135f6a60
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-15 21:15:55 +0530

    Learning Function

--- forloopmain.sh
+++ forloopmain.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
 R="\e[31m"
-G="\e[32m"
+G="\e32m"
 Y="\e[33m"
-B="\e[34m"
+B="\e34m"
 N="\e[0m"
 
 LOG_FOLDER="/var/log/shellscript-logs"
@@ -11,40 +12,40 @@ SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 package=("nginx" "python3" "mysql")
 
-mkdir -p "$LOG_FOLDER"
-echo " Script is executing started at $(date) " 
+mkdir -p "$LOG_FOLDER
+echo -e " script started executing at $B $(date) $N "
 
-USERID=$(id -u)
 
-if [ $USERID -ne 0 ] &>>$LOG_FILE
+# User validation
+
+USERID="$(id -u)"
+if [ $USERID -ne 0 ] | &>>"$LOG_FILE"
 then
-     echo -e " $R ERROR: $N Please run this script with Root User " | tee -a $LOG_FILE
+     echo -e " $R ERROR:: $N Please run this with $B Root User $N " | tee -a "$LOG_FILE"
      exit 1
 else
-     echo -e " You are Running with Root User $G Successfull $N " |  tee -a $LOG_FILE
+     echo -e " $G You are running with Root User $N "  | tee -a "$LOG_FILE"
 fi
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-         echo -e " $G $2 is installed..... $G Successfull $N " | tee -a $LOG_FILE
+         echo -e " $G $2 installation Successfull $N " | tee -a "$LOG_FILE"
     else
-         echo -e " $B $2 Installing is..... Failed o.. $Y Nothing to do $N " | tee -a $LOG_FILE
+         echo -e " $R $2 installation Failed $N " | tee -a "$LOG_FILE"
          exit 1
-    fi
-
+    
 }
 
-for package in ${package[@]}
+for package in "$@"
 do
-    dnf list installed "$package"  &>>"$LOG_FILE"
-    if [ $? -ne 0 ]
-    then
-         echo -e " $R "$package" not found... $G Going to Install $N " | tee -a "$LOG_FILE"
-         dnf install "$package" -y  &>>"$LOG_FILE"
-         VALIDATE $? "$package"
-    else echo -e " $G "$package" $N are already installed::::: $Y nothing to do $N " | tee -a "$LOG_FILE"
+   dnf list installed "$package" | &>>"$LOG_FILE"
+   if [ $? -ne 0 ]
+   then
+         echo -e " $R "$package" is Not Found... $Y Going to install $N "  | tee -a "$LOG_FILE"
+         dnf install "$package" -y  | &>>"$LOG_FILE"
+         VALIDATE "$?" "$package"
+    else
+         echo -e " $Y "$package" is already installed.... $B Nothing to do $N "  | tee -a "$LOG_FILE"
     fi
 done
-
-
