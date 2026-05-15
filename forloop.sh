f7617188b18fd0957ecade7cd4ef5bf04ce57868
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-15 05:30:50 +0530

    Learning Function

--- forloop.sh
+++ forloop.sh
@@ -16,19 +16,19 @@ echo -e " Script started executing at $B $(date) $N "
 USERID=$(id -u)
 if [ $USERID -ne 0 ] 
 then
-     echo -e " $R Error $N ::You are not running with Root User " | tee -a $LOG_FOLDER
+     echo -e " $R Error $N ::You are not running with Root User " | tee -a $LOG_FILE
      exit 1
 else
-     echo " You are running with Root User $G Successfull $N " | tee -a $LOG_FOLDER
+     echo " You are running with Root User $G Successfull $N " | tee -a $LOG_FILE
 fi
 
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-         echo -e " Installing $B $2 is .... $G Successfull $N " | tee -a $LOG_FOLDER
+         echo -e " Installing $B $2 is .... $G Successfull $N " | tee -a $LOG_FILE
     else
-         echo -e " Insgtalling $B $2 is ... $R Failed $N " | tee -a $LOG_FOLDER
+         echo -e " Insgtalling $B $2 is ... $R Failed $N " | tee -a $LOG_FILE
          exit 1
     fi 
 }
@@ -38,10 +38,10 @@ dnf list installed nginx
 if [ $? -ne 0 ]
 then
      echo " $2 is not found...Going to Install it "
-     dnf install nginx -y &>>$LOG_FOLDER
+     dnf install nginx -y &>>$$LOG_FILE
      VALIDATE $? "nginx"
 else
-     echo -e " $B $2 $N is already installed.... $Y Nothing to do $N " | tee -a $LOG_FOLDER
+     echo -e " $B $2 $N is already installed.... $Y Nothing to do $N " | tee -a $LOG_FILE
 fi
 
 dnf list installed python3
@@ -49,10 +49,10 @@ dnf list installed python3
 if [ $? -ne 0 ]
 then
      echo -e " $2 is not found...$G Going to Install it $N "
-     dnf install python3 -y &>>$LOG_FOLDER
+     dnf install python3 -y &>>$$LOG_FILE
      VALIDATE $? "python3"
 else
-     echo -e " $2 $N is already Installed....: $Y Nothing to do $N "  | tee -a $LOG_FOLDER
+     echo -e " $2 $N is already Installed....: $Y Nothing to do $N "  | tee -a $LOG_FILE
 fi
 
 dnf list installed mysql
@@ -60,9 +60,9 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
      echo -e " $2 is not found....$G Going to Install it $N "
-     dnf install mysql -y &>>$LOG_FOLDER
+     dnf install mysql -y &>>$LOG_FILE
      VALIDATE $? "mysql"
 else
-     echo -e " $2 $N is already Installed....: $Y Nothing to do $N " | tee -a $LOG_FOLDER
+     echo -e " $2 $N is already Installed....: $Y Nothing to do $N " | tee -a $LOG_FILE
 fi
 
