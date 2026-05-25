e80be38c40815c6af872cab8bb51126f2d3b410c
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-25 16:37:01 +0530

    Learning While loop

---
+++ backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Colours Formate
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+
+SOURCE_DIR="$1"
+DISTNATION_DIR="$2"
+DAYS="${3:-14}" # if days are provided that will be considered, otherwise daualt in 14 days.
+
+LOG_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME"
+
+mkdir -p $LOG_FOLDER
+
+# Root User Validation
+$USER_ID="$(id -u)"
+if [ $USER_ID -ne 0 ]
+then
+     echo -e "$R ERROR: $N Please Procced with Root User"
+     exit 1
+else
+     echo -e "$G You are Running With Root User $N"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+         echo -e "$G $2 is....Success $N"
+    else
+         echo -e "$R $2 is....Failed $N"
+         exti 1
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE: $N sh backup.sh <source_dir> <destination_dir> <days(optional)>"
+    exit 1
+}
+
+if [ $# -lt 2 ]
+then
+    $USAGE
+fi
+
