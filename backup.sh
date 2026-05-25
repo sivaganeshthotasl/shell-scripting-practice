9e1f63ee803fbc47bc31da0acbec8bc52b81105d
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-25 17:22:41 +0530

    Learning While loop

--- backup.sh
+++ backup.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-# Colours Formate
+# Colours Format
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
 
-
 SOURCE_DIR="$1"
-DISTNATION_DIR="$2"
-DAYS="${3:-14}" # if days are provided that will be considered, otherwise daualt in 14 days.
+DESTINATION_DIR="$2"
+DAYS=${3:-14}
 
 LOG_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOG_FOLDER
+mkdir -p "$LOG_FOLDER"
 
 # Root User Validation
+
 USER_ID="$(id -u)"
-if [ $USER_ID -ne 0 ]
+
+if [ "$USER_ID" -ne 0 ]
 then
-     echo -e "$R ERROR: $N Please Procced with Root User"
+     echo -e "$R ERROR: $N Please Proceed with Root User"
      exit 1
 else
      echo -e "$G You are Running With Root User $N"
 fi
 
 VALIDATE(){
-    if [ $1 -eq 0 ]
+    if [ "$1" -eq 0 ]
     then
          echo -e "$G $2 is....Success $N"
     else
@@ -56,25 +57,17 @@ fi
 
 if [ ! -d "$DESTINATION_DIR" ]
 then
-     echo -e "$R Destination Directory $destination_dir does not exist $N Please Check"
+     echo -e "$R Destination Directory $DESTINATION_DIR does not exist $N Please Check"
+     exit 1
 fi
 
+FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +"$DAYS")
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-
-if [ ! -z $FILES ]
+if [ ! -z "$FILES" ]
 then
      echo "Files to zip are: $FILES"
 
 else
-     echo -e "$R No Files Found Older then 14 days..Skipping $N"
+     echo -e "$R No Files Found Older than $DAYS days..Skipping $N"
      exit 1
-
 fi
-
-
-
-
-
-
-
