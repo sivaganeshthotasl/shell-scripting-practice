5679e5b28b0ff5bfbd17e9fc02d18b1b41568b18
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-26 10:49:04 +0530

    Learning Function

--- backup.sh
+++ backup.sh
@@ -63,13 +63,12 @@ fi
 
 FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +"$DAYS")
 
-if [ -n "$FILES" ]
+if [ ! -z "$FILES" ]
 then
      echo "Files to zip are: $FILES"
      TIME_STAMP="$(date +%F-%H-%M-%S)"
      ZIP_FILE="$DESTINATION_DIR/app-logs-$TIME_STAMP.zip"
      echo $FILES | zip -@ $ZIP_FILE
-
 
 else
      echo -e "$R No Files Found Older than $DAYS days..Skipping $N"
