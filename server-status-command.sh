bbe29923d45b04c78a6884bbe7616b0295ca026a
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-27 15:27:32 +0530

    Learning Function

--- server-status-command.sh
+++ server-status-command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Server Status"
+
 echo "Server Status: $(uptime)"
-echo "Server Disk Usage Status"
+
 echo "Disk Usage: $(df -hT)"
-echo "Memory Usage Status"
+
 echo "Memore Usage $(free -m)"
 
