c4a5b0512ee560aea343881790519d30bf3e290e
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-27 21:37:27 +0530

    Learning Function

---
+++ disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE="$(df -hT | grep -v filesystem)"
+DISK_THRESHOLD="1" # in project it will be 75 %
+
+
+
+while IFS= read line
+do
+     USAGE=(echo $line | awk '{print $6f}' | cut -d "%" -f1)
+     PARTITION=(echo $line | awk '{print $7f}')
+     echo "$PARTITION: $USAGE"
+
+done >>> $DISK_USAGE
