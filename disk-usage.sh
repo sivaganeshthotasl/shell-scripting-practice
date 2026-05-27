6f7ab4f38a40c79ba6fdb8069aaee0456cfade13
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-27 22:00:22 +0530

    Learning Function

--- disk-usage.sh
+++ disk-usage.sh
@@ -7,13 +7,12 @@ DISK_THRESHOLD="1" # in project it will be 75 %
 
 while IFS= read line
 do
-     USAGE=$(echo "$line" | awk '{print $6f}' | cut -d "%" -f1)
-     PARTITION=$(echo "$line" | awk '{print $7f}')
-     if [ $USAGE -ge $DISK_THRESHOLD ]
-     then
-            MSG="High Disk Usage On $PARTITION: $USAGE"
-     if 
-
+    USAGE=$(echo $line | awk '{print $6f}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | aws '{print $7f}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+         MSG="High Disk Usage on $PARTITION: $USAGE"
+    fi
 done <<< $DISK_USAGE
 
-echo "$MSG" 
+echo "$MSG"
