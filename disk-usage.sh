cf6c244771fcb49ae5de0d6b74e8d564e69051a0
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-28 10:18:02 +0530

    Learning Function

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+DISK_USAGE="$(df -hT | grep -v filesystem)"
 DISK_THRESHOLD=1
 
-df -hT | awk 'NR>1 {print $6, $7}' | while read -r USAGE PARTITION
-do
-    USAGE=$(echo "$USAGE" | cut -d "%" -f1)
 
-    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]
-    then
-        echo "High Disk Usage on $PARTITION: $USAGE%"
-    fi
+while IFS= read line
+do
+     USAGE="$(echo $line | awk '{print $6F}' | cut -d "%" -f1)"
+     PARTITION="$(echo $line | awk '{print $7F}')"
+     if [ $USAGE -ge $DISK_THRESHOLD ]
+     then
+        MSG+="High Disk Usage On: $PARTITION: $USAGE"
+     fi
+
+
 done
