701f352dd939dc383ab623646a1a07c9a4436161
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 22:07:35 +0530

    Learning Function

--- 26-appsetup-function.sh
+++ 26-appsetup-function.sh
@@ -51,11 +51,11 @@ APP_SETUP(){
     mkdir -p /app
     VALIDATE $? "Creating Application Directory"
     
-    ARTIFACT_URL="curl -o /tmp/$APP_NAME.zip https://roboshop-artifacts.s3.amazonaws.com/$APP_NAME-v3.zip" &>>$LOG_FILE
+    ARTIFACT_URL="curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip" &>>$LOG_FILE
     VALIDATE $? "Downloading $APP_NAME File into /tmp Directory"
     
     cd /app
-    unzip -o /tmp/$APP_NAME.zip &>>$LOG_FILE
+    unzip -o /tmp/catalogue.zip
     VALIDATE $? "Extracting $APP_NAME files"
 
 }
