92649b06f49a9d98b43380fb6989027d554f9716
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 19:52:38 +0530

    Learning Function

---
+++ 19-string-names.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# String Equal Condition
+
+# String Variable
+
+NAME="Siva"
+
+if [ "$NAME" = "Siva" ]
+then
+      echo "Names Are Matching....."
+else
+      echo "Names Are Not Matching....."
+fi
+
