87c0ab08113c2e5bbb4e1a884df0966905a52013
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 20:11:37 +0530

    Learning Function

--- 20-string-empty-check.sh
+++ 20-string-empty-check.sh
@@ -11,3 +11,13 @@ else
      echo "String is Not....EMPTY"
 fi
 
+# String not Empty check status
+
+# NAME="Siva"
+
+# if [ -z "$NAME" ]
+# then
+#      echo "String is...EMPTY"
+# else
+#      echo "String is Not....EMPTY"
+# fi
