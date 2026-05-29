5432cac66ed200901791fe2becfae2c55e264a07
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 10:32:59 +0530

    Learning Shell script

--- 4-user-input.sh
+++ 4-user-input.sh
@@ -3,7 +3,7 @@
 
 # Print User Name is :
 echo "Enter User Name:"
-read USERNAME   # USERNAME is variable
+read USERNAME   # read strotes user input into USERNAME variable. USERNAME is variable here.
 echo "Entered UserName is: "$USERNAME""
 
 
