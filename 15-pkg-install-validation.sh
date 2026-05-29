917d5e7bfdb19e7c47ade2f92b0e351100a4c95a
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 15:19:56 +0530

    Learning Shell script

---
+++ 15-pkg-install-validation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Package Install Validation
+
+# Logs Configuration Set Up
+
+LOG_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME="$(echo $0 | cut -d "." f1)"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+# Create a Log Directory to store sttdin and sttdout logs
+mkdir -p $LOG_FOLDER
+
+# Root User Validation
+USER_ID="$(id -u)"
+if [ $USER_ID -ne 0 ]
+then
+     echo "Please Run this Scritp with Root User"
+else
+     echo "You are Running with Root User"
+
+# VALIDATE FUNCATION
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+         echo "$2 is........SUCCESS"
+    else
+         echo "$2 is........FAILED"
+    fi
+}
+
+# Installing Nginx.
+PACKAGE="nginx"
+dnf install $PACKAGE -y &>>$LOG_FILE
+VALIDATE $? "Nginx is installing"
+
