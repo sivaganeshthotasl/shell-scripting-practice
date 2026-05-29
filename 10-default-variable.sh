336bab7b98e2d59084a3e4c490c5623b63d1bd71
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 11:38:05 +0530

    Learning Shell script

--- 10-default-variable.sh
+++ 10-default-variable.sh
@@ -2,9 +2,10 @@
 
 # Default Variables
 
-DAYS="${1:-14}" 
+DAYS="${1:-14}" # if user passes value 1 → use it like sh 10-default-variable.sh 20 it takes 20 day orelse deafult 14 days it will take.
 
-echo "$DAYS"
+
+echo "$DAYS" # Printing the user passed valued or default value
 
 
 
