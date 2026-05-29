d1404d13d4c94ee3c80a12a7d9c5509e20a543c8
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 10:23:41 +0530

    Learning Shell script

--- 3-data-variable.sh
+++ 3-data-variable.sh
@@ -2,9 +2,10 @@
 
 # Date Variable
 
-DATE="$(date)"
+DATE="$(date)"   # () we call this command substution. this is used to execute commands
 
 # Print Today Date
 
 echo "Today Date is: "$DATE""
+
 
