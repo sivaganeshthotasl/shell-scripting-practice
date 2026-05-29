45659e7f4543a649ce970f5a70877da710ff4623
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 21:00:32 +0530

    Learning Function

---
+++ 24-reusable-validate.fun.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Reusable Validate Function
+
+# Check Root User Validation
+
+USER_ID="$(id -u)"
+if [ $USER_ID -ne 0 ]
+then
+     echo "ERROR: Please Run This Script with Root User"
+     exit 1
+else
+     echo "You Are Running With Root User"
+fi
+
+
+# Create Validate Function
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+         echo "$2 is....SUCCESS"
+    else
+         echo "$2 is.....FAILED"
+         exit 1
+    fi
+
+}
+
+# To check Validate function install nginx and redis packages. i am using here array variables instead creating two saperate package name variables.
+
+PACKAGES=("nginx" "redis")
+
+for PACKAGE in "${PACKAGES[@]}"
+do
+    dnf install $PACKAGE -y
+    VALIDATE $? "$PACKAGES Installing"
+done
+
