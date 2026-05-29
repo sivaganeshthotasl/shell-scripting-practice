ac36fd24b9c108dbf9b23de8547013c49e940de4
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 14:50:52 +0530

    Learning Shell script

---
+++ 13-Intiger-function.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Basic Intiger Function
+
+# Create Number Variable
+
+NUMBER=10
+
+# Check if number is greater than 5
+
+if [ $NUMBER -ge 5 ]   
+then
+     echo "$NUMBER is Greater than 5" 
+else
+     echo "$NUMBER is Not greater than 5"
+fi
+
+
+
