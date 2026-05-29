c5182239ec751bf6a2d96258b556fea2eff78779
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 10:52:31 +0530

    Learning Shell script

--- 6-package-variables.sh
+++ 6-package-variables.sh
@@ -2,13 +2,26 @@
 
 # Package Installation Variables
 
-PACKAGE="nginx"
+# PACKAGE="nginx"
+
+# # Install Using Variable
+
+# dnf install "$PACKAGE" -y
+
+# # Print Installing nginx.......
+
+# echo "Installing "$PACKAGE".......SUCCESS"
+
+
+MYSQL_PACKAGE="mysql"
+REDIS_PACKAGE="redis"
+MONGODB_PACKAGE="mongodb"
 
 # Install Using Variable
-
-dnf install "$PACKAGE" -y
-
-# Print Installing nginx.......
-
-echo "Installing "$PACKAGE".......SUCCESS"
+dnf install "$MYSQL_PACKAGE" -y
+echo "Installing "$MYSQL_PACKAGE".......SUCCESS"
+dnf install "$REDIS_PACKAGE" -y
+echo "Installing "$REDIS_PACKAGE"........SUCCESS"
+dnf install "$MONGODB_PACKAGE" -y
+echo "Installing "$MONGODB_PACKAGE".......SUCCESS"
 
