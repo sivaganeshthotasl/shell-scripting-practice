f066f3c3465524cf3828be4e9d7ff20cc4d710b2
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 14:32:55 +0530

    Learning Shell script

---
+++ 12-mini-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Now Combine Evertying.
+
+COMPONENT="nginx"
+
+# Logs Setup
+LOG_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+# Create Log Folder
+mkdir -p $LOG_FOLDER
+
+echo "Deploying "$COMPONENT".........."
+
+dnf install $COMPONENT -y
+
+# Print Success Statement
+echo ""$COMPONENT" installing is.......SUCCESS"
+
