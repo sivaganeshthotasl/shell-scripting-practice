6a4f0b4b77803f60363709c655e47898e1b978e8
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 11:18:38 +0530

    Learning Shell script

---
+++ 8-appdeploy-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Application Deployment Variables
+
+APP_NAME="catalogue"
+APP_USER="roboshop"
+APP_DIR="/app"
+
+# Print Deployment Message
+
+echo "Deploying "$APP_NAME"........"
+
