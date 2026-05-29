d7c07c3940a5c0d5b88842ec5a94aa0184bd1539
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 10:39:52 +0530

    Learning Shell script

---
+++ 5-arthematic-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Create Two Variable.
+A="10"
+B="20"
+RESULT="$((A+B))"  #arithmetic expansion
+
+# Print Arthematic Variables
+
+echo "Addiont of 10 and 20 is: "$RESULT""   # echo formating
