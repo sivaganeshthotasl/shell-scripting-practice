2833bffa3bbb15b9835feab2d55b50913f4b9132
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 11:11:38 +0530

    Learning Shell script

--- 7-logfolder-variables.sh
+++ 7-logfolder-variables.sh
@@ -3,7 +3,7 @@
 # Create Log Folder Variables
 
 LOG_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME="$(echo $0 | basename "." sh)"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 LOG_FILE=""$LOG_FOLDER"/$SCRIPT_NAME.log"
 
 # Print Log File Path
