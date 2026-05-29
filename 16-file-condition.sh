dbbec460afddf13fa53dacd3b4995a8a77512008
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 15:50:24 +0530

    Learning Shell script

---
+++ 16-file-condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check whether File exits /etc/passwd
+
+# Create a File path Varialbe
+FILE_PATH="/etc/passwd"
+
+# Check wheter file extis
+
+if [ -f $FILE_PATH ]
+then
+     echo "File is Already Exist"
+else
+     echo "File is doesn't Exist"
+fi
