6ce780fcefb983e88f92f3040316305880e3a21d
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 20:05:35 +0530

    Learning Function

--- 19-string-names.sh
+++ 19-string-names.sh
@@ -2,15 +2,15 @@
 
 # String Equal Condition
 
-# String Variable
-
-NAME="$1" 
 
 # echo "Your Name is:" # Setting up User input
 
 # read NAME   # read is used to store user input 
 
-#NAME="SIVA"
+
+# String Variable
+NAME="Siva Ganesh Thota SL"
+
 if [ "$NAME" = "Siva Ganesh Thota SL" ]
 then
       echo "Names Are Matching....."
