ee9494567672c6e82559c267cf9ce403f559c0eb
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 11:22:57 +0530

    Learning Shell script

---
+++ 9-url-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# URL Variable
+
+# Store artifact url into variable
+
+ARTIFACT_URL="https://roboshop-builds.s3.amazonaws.com/catalogue.zip"
+
+
+# Download the Artifact url
+
+curl -o /tmp/catatalogue.zip $ARTIFACT_URL
+
