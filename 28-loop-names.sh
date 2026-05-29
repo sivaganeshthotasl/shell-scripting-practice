22101baa064284aa01ff0ffd05800ade57e3eb95
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-29 22:24:42 +0530

    Learning Function

--- 28-loop-names.sh
+++ 28-loop-names.sh
@@ -3,7 +3,7 @@
 
 for PACKAGES in nginx redis mysql
 do
-    echo "${PACKGES[0]} Installing...."
+    echo "${PACKAGES[0]} Installing...."
     echo "${PACKAGES[1]} Installing...."
     echo "${PACKAGES[2]} Installing...."
 done
