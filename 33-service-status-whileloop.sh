adb3e0d03375cafe4a70d9ab5eb0d8d227df3678
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-30 12:10:55 +0530

    Practing Shellscript

--- 33-service-status-whileloop.sh
+++ 33-service-status-whileloop.sh
@@ -2,16 +2,27 @@
 
 # Check if nginx is active
 
+# PACKAGE="nginx"
+
+# STATUS="$(systemctl is-active $PACKAGE)"
+
+# while [ "$STATUS" != "active" ]
+# do
+#       echo ""$PACKAGES" is not running"
+#       sleep 1
+
+#       STATUS="$(systemctl is-active $PACKAGE)"
+# done
+
+# echo ""$PACKAGE" is...RUNNING"
+
+
 PACKAGE="nginx"
+COUNT=1
 
-STATUS="$(systemctl is-active $PACKAGE)"
-
-while [ "$STATUS" != "active" ]
+while [ $PACKAGE -le 5 ]
 do
-      echo ""$PACKAGES" is not running"
-      sleep 1
-
-      STATUS="$(systemctl is-active $PACKAGE)"
+       STATUS="$(systemctl is-active nginx)"
+       echo "Attempt $COUNT : $STATUS"
+       COUNT="$(($COUNT+1))"
 done
-
-echo ""$PACKAGE" is...RUNNING"
