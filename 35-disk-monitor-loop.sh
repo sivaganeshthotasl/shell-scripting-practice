b68308954e5692c4740a92aff984eaa9fefa2581
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-30 15:27:06 +0530

    Practing Shellscript

--- 35-disk-monitor-loop.sh
+++ 35-disk-monitor-loop.sh
@@ -1,22 +1,15 @@
-#!/bin/bash
+!/bin/bash
 
-# df -hT | tail -n +2 | while read FILESYSTEM TYPE SIZE USED AVAIL USAGE MOUNTPOINT
-# do
-#        echo "Partition:  $MOUNTPOINT"
-#        echo "Usage: $USAGE"
-# done
-
-
-
-
-DISK_USAGE=$(df -hT | tail -n +2)
-
-while  [ $DISK_USAGE ]
+df -hT | tail -n +2 | while read FILESYSTEM TYPE SIZE USED AVAIL USAGE MOUNTPOINT
 do
-     echo "Partiion: $7"
-     echo "Usage: $6"
-
+       echo "Partition:  $MOUNTPOINT"
+       echo "Usage: $USAGE"
 done
+
+
+
+
+
 
 
 
