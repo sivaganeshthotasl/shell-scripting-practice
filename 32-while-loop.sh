a37a80d0bccccd6ee4dad4aa3bd0e0593a779540
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-30 12:25:36 +0530

    Practing Shellscript

---
+++ 32-while-loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Print Multiplication
+
+NUMBER=5
+
+while [ $NUMBER -le 50 ]
+do
+      RESULT=$((5*$NUMBER))
+      echo "5 x $NUMBER = $RESULT"
+
+      NUMBER=$(($NUMBER+1))
+
+done
+
