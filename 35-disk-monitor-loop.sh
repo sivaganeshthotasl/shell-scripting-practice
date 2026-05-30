f233f0ecf9df701d259ea87801513b143dbfa461
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-30 15:06:35 +0530

    Practing Shellscript

--- 35-disk-monitor-loop.sh
+++ 35-disk-monitor-loop.sh
@@ -2,9 +2,18 @@
 
 df -hT | tail -n +2 | while read FILESYSTEM TYPE SIZE USED AVAIL USAGE MOUNTPOINT
 do
-       echo "Partition: $MOUNTPOINT"
+       echo "Partition:  $MOUNTPOINT"
        echo "Usage: $USAGE"
 done
+
+USAGE_VALUE=$(echo $USAGE | cut -d "%" -f1)
+
+if [ $USAGE_VALUE -gt 30 ]
+then
+     echo "$MOUNTPOINT -> $USAGE"
+fi
+
+
 
 
 
