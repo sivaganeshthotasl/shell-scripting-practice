1fa07ca4233bea98b8f202bd6b492dd400994f05
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-31 12:24:53 +0530

    Learning Function

---
+++ 44-multiple-arg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Multiple arguments
+NAME="Siva Ganesh Thota SL"
+COMPANY="Benz"
+LOCATION="Singapore"
+
+# Print all arguments
+echo "My Name is $NAME"
+echo "I work in $COMPANY"
+echo "My Work Location is $LOCATION"
