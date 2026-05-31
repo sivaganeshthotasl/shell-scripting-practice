d549020bd86726fe846612db98bb7932fccf4139
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-31 11:33:12 +0530

    Learning Function

--- 41-trap-command.sh
+++ 41-trap-command.sh
@@ -2,7 +2,7 @@
 
 
 
-trap 'echo "ERROR Occured at line $LINENO"' ERR
+trap 'echo "ERROR occured at line $LINENO"' ERR  # this should be written starting from the script i mena before commands ti should be if you give this last it will not work.
 
 dnf install fakepackage -y # this is wrong package name so this script should stop here and say error.
 ls /acde
