0962bff5779a451606d9670120755f5bf5199630
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 16:13:45 +0530

    Learning Function

--- 51-directory-validation-task.sh
+++ 51-directory-validation-task.sh
@@ -3,12 +3,13 @@
 # Directory Validation
 # Start with Task 1: Directory Validation and write a script that checks whether /tmp exists and prints a success/failure message.
 
-TMP_DIR="/tmps"
+TMP_DIR="/tmp"
 if [ -d $TMP_DIR ]
 then
       echo "Directory "$TMP_DIR" is exist"
 fi
 
+DESTINATION_DIR="/destination_dir"   # i haven't created destination dir so it would get that does not exist.
 if [ ! -d $TMP_DIR ]
 then
       echo "Directory "$TMP_DIR" doesn't exit"
