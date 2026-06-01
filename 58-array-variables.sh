2a96388bbc386632acb82a15c6647deabc5065d0
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 23:09:23 +0530

    Learning Function

--- 58-array-variables.sh
+++ 58-array-variables.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Array Variable using arrays we can store multimple values. with out array we should create multiple variable names.
-PACKAGE=("nginx" "python3" "redis") # PACKAGE is a variable name and values are stored in variable. 
 
-# To access array variables we use "${PACKAGE[0]}" means that nginx is installing. here values counts from 0.
-echo "Installing "${PACKAGE[0]}""
-echo "Installing "${PACKAGE[1]}""
-echo "Installing "${PACKAGE[2]}""
+# Arrays allow us to store multiple values in a single variable.
+# Without arrays, we would need separate variables for each value.
 
-# OutPut shows
-# Installing nginx
-# Installing python3
-# Installing redis
+PACKAGE=("nginx" "python3" "redis")
+
+# Array indexing starts from 0.
+echo "Installing ${PACKAGE[0]}"
+echo "Installing ${PACKAGE[1]}"
+echo "Installing ${PACKAGE[2]}"
 
 
 
