17d0925af27f84e38a15c8088a5499bd27a160dd
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 23:31:41 +0530

    Learning Function

---
+++ 60-service-monitoring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Service Monitoring check
+
+SERVICES=("nginx" "sshd")
+
+for SERVICE in ${SERVICES[@]}
+do
+    systemctl is-active "$SERVICE"
+    # check whether the service is active or not
+    if [ $? -ne 0 ]
+    then
+         echo "Service is $SERVICE not Active"
+         systemctl start "$SERVICE"
+    else
+         echo "Service is $SERVICE is already in Active..Skipping"
+    fi
+done
