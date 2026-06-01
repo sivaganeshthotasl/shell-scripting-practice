7ca80c96858a7ce6f63d748e581b733d38b5bda4
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 11:41:09 +0530

    Learning Function

--- 49-loop-through-arg.sh
+++ 49-loop-through-arg.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 
-# Loop through arguments
+# Loop through arguments inside the script
 
 for PACKAGES in nginx redis mysql-server
 do
      echo "Installing $PACKAGES"
 done
 
+# Loop through arguments passing from the script
+USAGE(){
+    if [ $# -ge 1 ]
+    then
+        echo "Usage: sh "$0" <package name1> <package name2>"
+        exit 1
+    fi
+}
+
+USAGE
+
+for PACKAGES in "$@"   
+do
+     echo "Installing $PACKAGES"
+
+else
 
 
