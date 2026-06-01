9a9dfeb8f0c0512dbe68ea254979d14bd3364fa3
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 23:23:09 +0530

    Learning Function

--- 59-loop-through-arrays.sh
+++ 59-loop-through-arrays.sh
@@ -7,6 +7,14 @@ PACKAGES=("nginx" "python3" "redis")
 for PACKAGE in ${PACKAGES[@]}
 do
      echo "installing... $PACKAGE"
+     dnf install $PACKAGE -y
+     if [ $? -eq 0 ]
+     then
+          echo "$PACKAGE is installed Successfully"
+     else
+          echo "$PACKAGE Installaion...Failled"
+     fi
+     
 done
 
 
