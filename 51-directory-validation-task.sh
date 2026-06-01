dc1faab9afdbf4a4e5366d34575074ab0253d73a
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 16:14:29 +0530

    Learning Function

--- 51-directory-validation-task.sh
+++ 51-directory-validation-task.sh
@@ -10,9 +10,9 @@ then
 fi
 
 DESTINATION_DIR="/destination_dir"   # i haven't created destination dir so it would get that does not exist.
-if [ ! -d $TMP_DIR ]
+if [ ! -d $DESTINATION_DIR ]
 then
-      echo "Directory "$TMP_DIR" doesn't exit"
+      echo "Directory "$DESTINATION_DIR" doesn't exit"
       exit 1
 fi
 
