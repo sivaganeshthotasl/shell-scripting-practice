3994c09231e8f6076f9aed7eac8f2d061474b645
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 23:01:21 +0530

    Learning Function

--- 58-array-variables.sh
+++ 58-array-variables.sh
@@ -4,8 +4,15 @@ PACKAGE=("nginx" "python3" "redis") # PACKAGE is a variable name and values are 
 
 # To access array variables we use "${PACKAGE[0]}" means that nginx is installing. here values counts from 0.
 echo "Installing "${PACKAGE[0]}""
+echo "Installing "${PACKAGE[1]}""
+echo "Installing "${PACKAGE[2]}""
 
 # OutPut shows
 # Installing nginx
+# Installing python3
+# Installing redis
+
+
+
 
 
