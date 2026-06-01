cafa4520536367bfa456322a5b9fe5d84d18ef73
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 23:41:17 +0530

    Learning Function

---
+++ 61-project-components.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+COMPONENTS=("Frontend" "Catalogue" "MongoDB" "Redis" "User" "Cart" "Mysql" "Shipping" "Rabbitmq" "Payment" "Dispatch")
+
+for COMPONENT in "${COMPONENT[@]}"
+do
+      echo "Deploying "$COMPONENT""
+done
+
