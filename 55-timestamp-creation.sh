a0a85aa86cb8fdff102e36b7acb269c70bdcf4c3
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 19:25:52 +0530

    Learning Function

--- 55-timestamp-creation.sh
+++ 55-timestamp-creation.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# Time Stamp Creation
+# Time Stamp Creation and create a zip file with the name time stamp
 
 FILE_PATH="/app-log"
 DESTINATION_DIR="/destinationdir"
 DAYS="${3:-14}"
 
-FILE="$(find $FILE_PATH -name "*.log" -mtime +$DAYS)"
-
+FILES="$(find $FILE_PATH -name "*.log" -mtime +$DAYS)"
 TIME_STAMP="$(date +%F-%H-%M-%S)"
-
-
 ZIP_FILE="$DESTINATION_DIR/app-log-$TIME_STAMP.zip"
 
-find $FILE_PATH -name "*.log" -mtime +$DAYS | zip -@ $ZIP_FILE
+if [ -n "$FILES" ]
+then
+     echo "Files $FILES are found"
+     find "$FILE_PATH" -name "*.log" -mtime +$DAYS | zip -@ $ZIP_FILE
+     echo "Zip file created: $ZIP_FILE"
+else
+     echo "Files $FILES are not found"
+     exit 1
+fi
 
-echo "zip files are created at: $DESTINATION_DIR"
+
+
