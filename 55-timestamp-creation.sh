8fe6e3463e4ae8414694313a77b29d720b705b59
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 19:29:09 +0530

    Learning Function

--- 55-timestamp-creation.sh
+++ 55-timestamp-creation.sh
@@ -6,14 +6,15 @@ FILE_PATH="/app-log"
 DESTINATION_DIR="/destinationdir"
 DAYS="${3:-14}"
 
-FILES="$(find $FILE_PATH -name "*.log" -mtime +$DAYS)"
+FILES="$(find "$FILE_PATH" -name "*.log" -mtime +"$DAYS")"
 TIME_STAMP="$(date +%F-%H-%M-%S)"
 ZIP_FILE="$DESTINATION_DIR/app-log-$TIME_STAMP.zip"
 
 if [ -n "$FILES" ]
 then
-     echo "Files $FILES are found"
-     find "$FILE_PATH" -name "*.log" -mtime +$DAYS | zip -@ $ZIP_FILE
+     echo "Old Logs are found"
+     echo "$FILES"
+     find "$FILE_PATH" -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
      echo "Zip file created: $ZIP_FILE"
 else
      echo "Files $FILES are not found"
