63493d941873c6dffc6777a44094c8f1a78970dc
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 11:44:19 +0530

    Learning Function

--- 49-loop-through-arg.sh
+++ 49-loop-through-arg.sh
@@ -10,7 +10,7 @@
 
 # Loop through arguments passing from the script
 USAGE(){
-    if [ $# -le 1 ]
+    if [ $# -eq 0 ]
     then
         echo "Usage: sh "$0" <package name1> <package name2>"
         exit 1
