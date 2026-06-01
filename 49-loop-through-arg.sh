ec88382c750d1249a8db38595f90fecc7d600f28
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 15:13:07 +0530

    Learning Function

--- 49-loop-through-arg.sh
+++ 49-loop-through-arg.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 
-# Loop through arguments inside the script
+# # Loop through arguments inside the script
 
-# for PACKAGES in nginx redis mysql-server
-# do
-#      echo "Installing $PACKAGES"
-# done
+# # for PACKAGE in nginx redis mysql-server
+# # do
+# #      echo "Installing "$PACKAGE""
+#        dnf install "$PACKAGE"
+# # done
 
 # Loop through arguments passing outside from the script
+
+
 USAGE(){
     if [ $# -eq 0 ]
     then
@@ -16,6 +19,9 @@ USAGE(){
         exit 1
     fi
 }
+# Meaning:
+# if no arguments passed
+# stop script
 
 # Call the function if the argument not passed 
 USAGE "$@"   #  This passes all script arguments to the function.
---
+++ 50-cleanup-sample.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# zip the old file which has 14 day age to destination dir 
+# Delete the Old Files from the source dir
+
+#Start with Directory Variable
+SOURCE_DIR="/source_dir"
+DESTINATION_DIR="/destination_dir"
+DAYS="${3:-14}"
+
+# Validate if source dir exist
+if [ -d "$SOURCE_DIR" ]
+then
+     echo "Source Directory "$SOURCE_DIR" doesn't exist"
+     exit 1
+fi
+
+# Validate if destination Dir exist
+if [ -d "$DESTINATION_DIR"]
+then
+     echo "Destination Dir "$DESTINATION_DIR" doesn't exist"
+     exit 1
+fi
+
+# Find LOG files older then 14 days from source directory
+FILES="$(find "$SOURCE_DIR" -name "*.log" -mtime +$DAYS )"
+
+
+if [ -n "$FILES" ]
+then
+       echo "Files are zip to.."
+       # print list of selected file to backup
+       echo "$FILES"
+       # Generate time statmp for zip file name
+       TIME_STAMP="$(date +%F-%H-%M-%S)"
+       # Create zip file path using destination directory and timestamp
+       ZIP_FILE="$DESTINATION_DIR"/app-log."$TIME_STAMP"
+       # Pass log files to zip command using find command and pipe
+       find "$DESTINATION_DIR" -name "*.log" -mtime +$DAYS | zip -@ $ZIP_FILE
+       echo "zip files are created at: $ZIP_FILE"
+       # check wheter zip exist or not
+       if [ -n $ZIP_FILE]
+       then 
+             echo "Zip files are created successfully"
+       else
+             echo "Zip files are not created successfully"
+             exit 1
+       fi
+else
+      echo "No Log Files found older then $DAYS days...Skipping"
+fi
+
+
+
+
