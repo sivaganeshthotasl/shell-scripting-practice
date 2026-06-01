f5d18a79dc36333cc75c269bc468b320d099662e
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-01 23:34:44 +0530

    Learning Function

--- 60-service-monitoring.sh
+++ 60-service-monitoring.sh
@@ -12,11 +12,7 @@ do
     then
          echo "Service is $SERVICE not Active"
          systemctl start "$SERVICE"
-         if [ $? eq 0 ]
-         then
-              echo "Starting $SERVICE ...ACTIVATED"
-         fi
-         
+
     else
          echo "Service is $SERVICE is already in Active..Skipping"
     fi
