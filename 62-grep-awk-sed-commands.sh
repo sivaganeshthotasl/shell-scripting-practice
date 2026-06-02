b5f50585d0806ace59b73df155b5b190d0183cb1
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-06-02 16:03:17 +0530

    Learning Function

--- 62-grep-awk-sed-commands.sh
+++ 62-grep-awk-sed-commands.sh
@@ -10,7 +10,7 @@ echo "$COMMAND"
 
 # Remove File System Header
 
-vCOMMAND="$(df -hT | grep -v Filesystem | awk '{print $0}')"
+VCOMMAND="$(df -hT | grep -v Filesystem | awk '{print $0}')"
 
-echo "vCOMMAND"
+echo "VCOMMAND"
 
